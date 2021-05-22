b5d628d5064d39ced96cfc251e93b0d25fc10496
Andrew Young <andrew@vaelen.org>
2021-05-23 02:57:27 +0900

    Fixed typo in copyright notice.

--- src/usb.rs
+++ src/usb.rs
@@ -14,7 +14,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+    along with the AR2300 library.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 use rusb::{Device, GlobalContext};
