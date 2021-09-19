e384b7200ed33412c9ea134b7625aa1301d7c9ee
Andrew Young <andrew@vaelen.org>
2021-09-19 21:53:22 +0900

    Fixed copyright header

--- lib/src/queue.rs
+++ lib/src/queue.rs
@@ -1,3 +1,22 @@
+/*
+    Copyright 2021, Andrew C. Young <andrew@vaelen.org>
+
+    This file is part of the AR2300 library.
+
+    The AR2300 library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the AR2300 library.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex, Condvar};
 use std::collections::VecDeque;
